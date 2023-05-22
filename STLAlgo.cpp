ee7880a4b72cfb27fd52dea7fe5e486a5b23c65a
sameer patil <sbpatil29@gmail.com>
2023-05-22 16:39:50 +0530

    adding fort algo

--- STLAlgo.cpp
+++ STLAlgo.cpp
@@ -8,6 +8,8 @@ int main(){
     int arr[5]={1,2,3,4,5};
     int arr2[2]={1,2};
 
+    int arr3[5]={3,1,2,4,5};
+
     int dst[7];
     int *ptr=find(arr,arr+5,5);
 
@@ -22,6 +24,12 @@ int main(){
 
     merge(arr,arr+5,arr2,arr2+2,dst);
 
+    sort(arr3,arr3+5);
+
+    for(int i=0;i<5;i++)
+        cout<<arr3[i];
+
+    cout<<endl;
     for(int i=0;i<7;i++)
     cout<<dst[i]<<" ";
 
